1a92eed3ec81aa355dab923f71304f6f06c90583
Jangchulwoon <lusiue@naver.com>
2018-06-17 21:40:35 +0900

    add bad small code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,16 @@
 package main.java;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        List<String> list = Arrays.asList("hello","world");
+        String str1 = "";
+        for(String str : list){
+            str1 += str;
+        }
+        System.out.println(str1.toString());
     }
 }
