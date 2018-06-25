a6cc4c2cdbf067a28fb4d94ef8d9585ececfca47
Jangchulwoon <lusiue@naver.com>
2018-06-25 21:51:45 +0900

    make request ~

--- src/main/java/message/Response.java
+++ src/main/java/message/Response.java
@@ -2,33 +2,82 @@ package main.java.message;
 
 
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Response implements Serializable{
     private int code;
+    private String version;
+    private String status;
     private Map headers;
     private String contents;
 
-    public int getCode() {
-        return code;
+    public static Response init(){
+        Response response = new Response();
+        // hard coding
+        response.setCode(200).setStatus("OK").setVersion("HTTP/1.1");
+        response.setHeaders(response.initHeader()).setContents("<html><header><title>TEst</title></header><body>hello</body></html>");
+
+        return response;
     }
 
-    public void setCode(int code) {
+    public Map initHeader(){
+        Map<String,String> map = new HashMap();
+        map.put("Access-Control-Allow-Origin","*");
+        return map;
+    }
+
+    public Response setCode(int code) {
         this.code = code;
+        return this;
+    }
+
+    public Response setVersion(String version) {
+        this.version = version;
+        return this;
+    }
+
+    public Response setStatus(String status) {
+        this.status = status;
+        return this;
+    }
+
+    public Response setHeaders(Map headers) {
+        this.headers = headers;
+        return this;
+    }
+
+    public Response setContents(String contents) {
+        this.contents = contents;
+        return this;
+    }
+
+    public int getCode() {
+        return code;
     }
 
     public String getContents() {
         return contents;
     }
 
-    public void setContents(String contents) {
-        this.contents = contents;
+    public String getVersion() {
+        return version;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Map getHeaders() {
+        return headers;
     }
 
     @Override
     public String toString() {
-        return code+"\n"+
-                "contentsize : 100"+
-                "\n\n" + contents;
+        return version +" " + code + " " + status +"\n"
+                + "Access-Control-Allow-Origin : * \n"
+                +"\n"
+                + contents;
+
     }
 }
--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -3,11 +3,9 @@ package main.java.server;
 
 import lombok.extern.java.Log;
 import main.java.message.Request;
+import main.java.message.Response;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -23,10 +21,17 @@ public class Server {
 
     private static void connetClient() {
         try (Socket socket = new ServerSocket(PORT).accept()) {
-            OutputStream out = socket.getOutputStream();
+            // input
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             Request request = Request.init(in);
-            System.out.println(request.toString());
+            // - input
+
+            // output
+            OutputStream out = socket.getOutputStream();
+            Response response = Response.init();
+            DataOutputStream dos = new DataOutputStream(out);
+            dos.writeBytes(response.toString());
+            dos.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
